b5709c4f42214ae01ebe7720d5ec4ca42a23fad7
unknown <daniel.preuss93@gmail.com>
2022-08-04 20:36:45 +0200

    Added premake file to glfw

---
+++ premake5.lua
@@ -0,0 +1,100 @@
+project "glfw"
+
+	kind "StaticLib"
+	staticruntime "off"
+	language "C"
+
+	targetdir (bindir .. "/%{prj.name}")
+	objdir (intdir .. "/%{prj.name}")
+
+	files
+	{
+		"include/GLFW/*.h",
+
+		"src/internal.h",
+		"src/platform.h",
+		"src/mappings.h",
+
+		"src/context.c",
+		"src/init.c",
+		"src/input.c",
+		"src/monitor.c",
+		"src/platform.c",
+		"src/vulkan.c",
+		"src/window.c",
+		"src/egl_context.c",
+		"src/osmesa_context.c",
+
+		"src/null_platform.h",
+		"src/null_joystick.h",
+
+		"src/null_init.c",
+		"src/null_monitor.c",
+		"src/null_window.c",
+		"src/null_joystick.c",	
+	}
+
+
+	filter "system:windows"
+
+		systemversion "latest"
+
+		files
+		{
+			"win32_time.h",
+			"win32_thread.h",
+			"win32_module.c",
+			"win32_time.c",
+			"win32_thread.c",
+
+			"src/win32_platform.h",
+			"src/win32_joystick.h",
+			"src/win32_init.c",
+			"src/win32_joystick.c",
+			"src/win32_monitor.c",
+			"src/win32_window.c",
+			"src/wgl_context.c",
+		}
+
+		defines
+		{
+			"_GLFW_WIN32",
+		}
+
+	filter "system:linux"
+
+		systemversion "latest"
+
+		files 
+		{
+			"src/posix_time.h",
+			"src/posix_thread.h",
+			"src/posix_module.c",
+			"src/posix_time.c",
+			"src/posix_thread.c",
+
+			"src/x11_platform.h",
+			"src/xkb_unicode.h",
+			"src/x11_init.c",
+			"src/x11_monitor.c",
+			"src/x11_window.c",
+			"src/xkb_unicode.c",
+			"src/glx_context.c",
+
+			"src/linux_joystick.h",
+			"src/linux_joystick.c",
+
+			"src/posix_poll.h",
+			"src/posix_poll.c",
+		}
+
+
+	filter "configurations:Debug"
+
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+
+		runtime "Release"
+		optimize "on"
